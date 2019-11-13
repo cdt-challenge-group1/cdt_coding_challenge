4e4e87ad2fe4e086e99b5938fde6866dba442ed6
James Andrew Major <jam86@aber.ac.uk>
2019-11-13 10:36:58 +0000

    Add code to request data and filter for a number of days in the past

---
+++ gatherData.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import requests
+import datetime
+import xlrd
+
+"""
+Request the price data from eni and return an array with tuples of
+the date and price for days less than daysAgo. The data does not contain
+prices for weekends so the number of prices returned may be less than
+daysAgo.
+"""
+def requestData(daysAgo):
+	url = "https://www.eia.gov/dnav/pet/hist_xls/RWTCd.xls"
+	agoDate = datetime.datetime.now() - datetime.timedelta(days=daysAgo)
+
+	data = requests.get(url)
+	if data.status_code != 200:
+		raise ValueError("Failed to get data")
+
+	workbook = xlrd.open_workbook(file_contents=data.content)
+	sheet = workbook.sheet_by_name("Data 1")
+	# https://blogs.harvard.edu/rprasad/2014/06/16/reading-excel-with-python-xlrd/
+	requestedData = []
+	# The sheet starts at row 3
+	for row_idx in range(3, sheet.nrows):
+		epoch = datetime.datetime.strptime('1990-01-01T00:00:00+0000', "%Y-%m-%dT%H:%M:%S%z")
+		# https://stackoverflow.com/questions/26010455/convert-xldate-to-python-datetime
+		date = datetime.datetime(*xlrd.xldate_as_tuple(sheet.cell(row_idx, 0).value, workbook.datemode))
+		if date >= agoDate:
+			requestedData.append((date, float(sheet.cell(row_idx, 1).value)))
+
+	return requestedData
+
+print(requestData(30))
