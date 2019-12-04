7ba6ccfe4317d80131733bf3301f07310fc5aad9
Robbie Webbe <robbie.webbe@bristol.ac.uk>
2019-12-04 10:09:24 +0000

    Rough version of ARIMA program.

---
+++ ARIMA_implementation_rough.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 09:45:37 2019
+
+@author: do19150
+"""
+
+import pandas as pd
+from statsmodels.tsa.arima_model import ARIMA
+import matplotlib.pyplot as plt
+
+#import the data set into a pandas DataFrame
+data = pd.read_csv('Insert name of data path', index_col=0)
+prices = data["Price"]
+
+#Initialise the parameters for the ARIMA model (p,d,q).
+order = (1,1,1)
+model = ARIMA(prices, order)
+
+#Fit the model to the data provided
+fit = model.fit()
+
+#Forecast the next price
+price_prediction = fit.forecast()[0]
+price_std = fit.forecast()[1]
+
