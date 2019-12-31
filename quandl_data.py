a5f03193f50f26ed3a39a055347630253ca4bad4
James Andrew Major <jam86@aber.ac.uk>
2019-12-31 16:01:12 +0000

    Add quandl data gathering

---
+++ quandl_data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from pandas.tseries.offsets import BDay
+import datetime
+import csv
+import requests
+from io import StringIO
+
+def get_quandl_data(daysAgo = None):
+	url = "https://www.quandl.com/api/v3/datasets/EIA/PET_RWTC_D.csv?api_key=2Q1jAB4TAk3a2pKq925R"
+	
+	if daysAgo != None and daysAgo > 0:
+		today = pd.datetime.today()
+		agoDate = today - BDay(daysAgo + 1)
+		fromDate = str(agoDate.date())
+		toDate = str((today - datetime.timedelta(days=1)).date())
+		url += "&start_date=" + fromDate + "&end_date=" + toDate
+	
+	data = requests.get(url)
+	if data.status_code != 200:
+		raise ValueError("Failed to get data")
+	data = data.content
+	
+	returnData = {}
+	reader = csv.reader(StringIO(data.decode("UTF-8")))
+	next(reader)
+	for row in reader:
+		returnData[row[0]] = row[1]
+	
+	return returnData
+
+print(get_quandl_data(30))
