9fd9d0fd87e9bbf70b7f4ab91baff4806a589ebd
James Andrew Major <jam86@aber.ac.uk>
2020-01-16 11:39:32 +0000

    Predict multiple days. Add delay cost and number of barrels

--- ARIMA_implementation_rough.py
+++ ARIMA_implementation_rough.py
@@ -10,25 +10,42 @@ import pandas as pd
 from statsmodels.tsa.arima_model import ARIMA
 import matplotlib.pyplot as plt
 import quandl_data as qd
+from pandas.tseries.offsets import BDay, DateOffset
 
 def ARIMA_predict():
     #import the data set into a pandas DataFrame
     #data = pd.read_csv('Insert name of data path', index_col=0)
-    data_days = 30
+    data_days = 60
     quandl_data = qd.get_quandl_data(data_days)
     
-    # TODO: check the last input day for days away from the current day and predict extra values if missing
+    # quandl has missing data do predict extra days
+    diffDays = pd.date_range(quandl_data[0][-1], pd.datetime.today(), freq=BDay()).size
+    print("Predicting %d missing days" % diffDays)
 
+    # predict the missing number of days and then the current day so that the prediction is for tomorrow not
+    # for the day after the data has days for
     #Initialise the parameters for the ARIMA model (p,d,q).
     order = (5,1,0)
     model = ARIMA(quandl_data[1], order=order)
 
     #Fit the model to the data provided
     fit = model.fit()
-
+    date = pd.datetime.today() - BDay(diffDays)
+    predicted_stdevs = []
+    for i in range(diffDays):
+        #Forecast the next price
+        price_prediction = fit.forecast()[0][0]
+        price_std = fit.forecast()[1][0]
+        
+        datestr = date.date().strftime("%Y-%m-%d")
+        quandl_data[0].append(datestr)
+        quandl_data[1].append(price_prediction)
+        predicted_stdevs.append(price_std)
+        date += BDay(1)
+    
     #Forecast the next price
-    price_prediction = fit.forecast()[0]
+    price_prediction = fit.forecast()[0][0]
     price_std = fit.forecast()[1]
-
-    return price_prediction, price_std, quandl_data
+        
+    return price_prediction, price_std[0], quandl_data, predicted_stdevs
 
--- dock_advice.py
+++ dock_advice.py
@@ -1,12 +1,16 @@
 from ARIMA_implementation_rough import ARIMA_predict
 import logging
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.tseries.offsets import BDay, DateOffset
 
-DELAY_COST = 0 # Replace this with actual delay cost
+DELAY_COST = 30000
+NUM_BARRELS = 750000
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 if __name__ == "__main__":
-    price_prediction, price_std, prices = ARIMA_predict()
+    price_prediction, price_std, prices, predicted_stdevs = ARIMA_predict()
     print(price_prediction, prices)
     price_today = prices[1][-1]
 
@@ -14,7 +18,7 @@ if __name__ == "__main__":
     if price_today >= price_prediction:
         dock_today = True
     # If the prediction is higher, check whether the delay cost is worth it
-    elif price_prediction + DELAY_COST > price_today: 
+    elif (price_prediction * NUM_BARRELS) + DELAY_COST > (price_today * NUM_BARRELS):
         dock_today = False
     else:
         dock_today = True
@@ -24,3 +28,11 @@ if __name__ == "__main__":
     else:
         logging.info('The boat should wait to dock')
 
+    ys = prices[1] + [price_prediction]
+    xs = prices[0] + [(pd.datetime.today() + BDay(1)).strftime("%Y-%m-%d")]
+    
+    errors = ([0] * (len(prices[0]) - len(predicted_stdevs))) + predicted_stdevs + [price_std]
+    plt.figure(figsize=(20,10))
+    plt.errorbar(xs, ys, yerr=errors, elinewidth=2)
+    plt.xticks(rotation=70)
+    plt.savefig("out.png")
