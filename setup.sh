f139fa72340b89750d0513612a3edb03b85e5c43
James Major <jam86@aber.ac.uk>
2020-03-03 19:17:10 +0000

    Fix pip install

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt install apache2 &&
-sudo apt install php &&
+sudo apt -y install apache2 php python3 python3-pip &&
 sudo cp 100-cdt_coding_challenge.conf /etc/apache2/sites-available &&
 sudo a2dissite 000-default &&
 sudo a2ensite 100-cdt_coding_challenge &&
 sudo service apache2 restart &&
 sudo mkdir -p /var/www/html/cdt/ &&
 cp -R . /var/www/html/cdt/ &&
-sudo chown -R www-data:www-data /var/www
+sudo chown -R www-data:www-data /var/www &&
+sudo pip3 install -r model/requirements.txt
