9c2ba35fe32db5cd2327cc2a834a9f15c99a04c4
James Major <jam86@aber.ac.uk>
2020-03-11 23:24:35 +0000

    Add basic web ui

--- frontend/index.php
+++ frontend/index.php
@@ -1,4 +1,19 @@
 <?php
 
-echo phpinfo();
-
+if (isset($_GET["run"])) {
+	$output = shell_exec("python3 " . __DIR__ . "../model/dock_advice.py 2>/dev/null");
+	/*$matches;
+	preg_match("/^The boat should (.*) today$/", $output, $matches);
+	$shouldDock = $matches[1] == "should";*/
+	
+	?><pre><?php echo $output; ?></pre><?php
+} else if (isset($_GET["graph"])) {
+	$data = file_get_contents("prediction_graph_output.png") or die("Failed to get graph");
+	echo $data;
+	die();
+} else {
+	?>
+	<a href=".?run">Run</a><br/>
+	<a href=".?graph">Graph (run first, large download)</a>
+	<?php
+}
