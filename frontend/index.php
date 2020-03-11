80cf610e3169b098aa9721018ee5a9798ff51b1e
James Major <jam86@aber.ac.uk>
2020-03-11 23:37:46 +0000

    Fix not being able to run the code from the ui. Fix not being able to see the graph

--- frontend/index.php
+++ frontend/index.php
@@ -1,16 +1,19 @@
 <?php
 
 if (isset($_GET["run"])) {
-	$output = shell_exec("python3 " . __DIR__ . "../model/dock_advice.py 2>/dev/null");
+	$output = shell_exec("python3 " . __DIR__ . "/../model/dock_advice.py 2>/dev/null");
 	/*$matches;
 	preg_match("/^The boat should (.*) today$/", $output, $matches);
 	$shouldDock = $matches[1] == "should";*/
 	
 	?><pre><?php echo $output; ?></pre><?php
 } else if (isset($_GET["graph"])) {
-	$data = file_get_contents("prediction_graph_output.png") or die("Failed to get graph");
+	/*$data = file_get_contents("prediction_graph_output.png") or die("Failed to get graph");
 	echo $data;
-	die();
+	die();*/
+	?>
+	<img src="prediction_graph_output.png" />
+	<?php
 } else {
 	?>
 	<a href=".?run">Run</a><br/>
