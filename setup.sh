e0577455928c690eab6f5abd6e94f1ab72787547
James Major <jam86@aber.ac.uk>
2020-04-01 15:02:44 +0100

    Use the correct url for running

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ sudo mkdir -p /var/www/html/cdt/ &&
 cp -R . /var/www/html/cdt/ &&
 sudo chown -R www-data:www-data /var/www &&
 sudo pip3 install -r model/requirements.txt &&
-wget -O /dev/null -o /dev/null 127.0.0.1 2>&1 /dev/null
+wget -O /dev/null -o /dev/null 127.0.0.1?run 2>&1 /dev/null
